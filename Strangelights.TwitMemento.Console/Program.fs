f482c7cb5aad44c9503157ebc6fc6add97e51a21
Robert Pickering <robert@strangelights.com>
2011-11-04 11:37:26 +0000

    console now working

--- Strangelights.TwitMemento.Console/Program.fs
+++ Strangelights.TwitMemento.Console/Program.fs
@@ -1,2 +1,9 @@
-﻿// Learn more about F# at http://fsharp.net
+﻿open Strangelights.TwitMemento
+open System
 
+let oauth_token, oauth_token_secret, usernameText = StoredOAuth.getOAuth (fun _ -> Console.ReadLine(), "robertpi")
+let incoming = new TwitterStreamFollower(usernameText, 5, oauth_token, oauth_token_secret)
+incoming.NewParsedTweet.Add (fun x -> printfn "%s %s" x.User.UserName x.Status)
+incoming.StartListening()
+Console.ReadLine() |> ignore
+incoming.StopListening()
